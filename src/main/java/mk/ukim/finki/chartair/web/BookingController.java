5cf0634389235716a74aaec5bdf8e33d3816c9f9
gjorshoskaivana <63347774+gjorshoskaivana@users.noreply.github.com>
2021-02-24 11:37:25 +0100

    Update on templates

--- src/main/java/mk/ukim/finki/chartair/web/BookingController.java
+++ src/main/java/mk/ukim/finki/chartair/web/BookingController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = {"/booking"})
 public class BookingController {
+
     @GetMapping
     public String getBookingPage(Model model) {
         model.addAttribute("bodyContent", "booking");
         return "master-template";
     }
+
 }
